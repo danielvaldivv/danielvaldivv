e9fedbd96a57e84d9ba8b3064941da3e0ab60576
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-01 12:21:37 -0500

    [Add] config for Webpack using svg file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import '../styles/Footer.css'
+import Linkedin from '../assets/icons/linkedin.svg'
+import GitHub from '../assets/icons/github.svg'
+import Platzi from '../assets/icons/platzi.svg'
+import Twitter from '../assets/icons/twitter.svg'
 
 function Footer() {
   return (
     <footer className="index-footer">
         <ul className="index-footer-contact">
-            <li><a href="https://www.linkedin.com/in/danielvaldivv/"><span><img src="../assets/icons/icons/linkedin.svg" alt="Logo LinkedIn"/></span></a></li>
-            <li><a href="https://github.com/danielvaldivv"><span><img src="../assets/icons/icons/github.svg" alt="Logo GitHub"/></span></a></li>
-            <li><a href="https://platzi.com/p/danielvaldivv/"><span><img src="../assets/icons/icons/platzi.svg" alt="Logo Platzi"/></span></a></li>
-            <li><a href="https://twitter.com/danielvaldivv/"><span><img src="../assets/icons/icons/twitter.svg" alt="Logo Twitter"/></span></a></li>
+            <li><a href="https://www.linkedin.com/in/danielvaldivv/"><span><img src={Linkedin} alt="Logo LinkedIn"/></span></a></li>
+            <li><a href="https://github.com/danielvaldivv"><span><img src={GitHub} alt="Logo GitHub"/></span></a></li>
+            <li><a href="https://platzi.com/p/danielvaldivv/"><span><img src={Platzi} alt="Logo Platzi"/></span></a></li>
+            <li><a href="https://twitter.com/danielvaldivv/"><span><img src={Twitter} alt="Logo Twitter"/></span></a></li>
         </ul> 
     </footer>
   )
