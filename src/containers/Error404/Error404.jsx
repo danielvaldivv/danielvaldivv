71d90442dfd10869aedc57c446b20c92684326c8
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-03 13:31:21 -0500

    [Add] Page Error 404 Not Found

---
+++ src/containers/Error404/Error404.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Error404.css'
+
+import arrowLeft from '../../assets/icons/arrow-left.png'
+
+function Error404() {
+  return (
+    <div className="Error404">
+      <h3>Ups... This page does´t exist</h3>
+      <h1>404</h1>
+      <h2>Not Found</h2>
+      <h3>Click here and go back</h3>
+      <Link to="/"><img  src={arrowLeft} alt="Arrow come back" width="50px" height="auto" /></Link>
+    </div>
+  )
+}
+
+export default Error404
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Error404 from '../containers/Error404/Error404';
 import Home from '../containers/Home/Home';
 
 import './App.css'
@@ -8,6 +9,7 @@ const App = () => (
   <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route  component={Error404}/>
       </Switch>
   </BrowserRouter>
 )
