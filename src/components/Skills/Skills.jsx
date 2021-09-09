76a179600211e598cad00fc638f5a8370d109739
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-09 09:23:06 -0500

    [Add] Cleaning code skills component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,41 +1,30 @@
 import React from 'react'
-import html from '../../assets/icons/skills/html.png'
-import css from '../../assets/icons/skills/css.png'
-import js from '../../assets/icons/skills/javascript.png'
-import react from '../../assets/icons/skills/react.png'
-import node from '../../assets/icons/skills/node.png'
-import webpack from '../../assets/icons/skills/webpack.png'
-import eslint from '../../assets/icons/skills/eslint.png'
-import prettier from '../../assets/icons/skills/prettier.png'
-import git from '../../assets/icons/skills/git.png'
-import github from '../../assets/icons/skills/github.png'
-import stylus from '../../assets/icons/skills/stylus.png'
-import sass from '../../assets/icons/skills/sass.png'
-import less from '../../assets/icons/skills/less.png'
-import firebase from '../../assets/icons/skills/firebase.png'
 
+import dataSkills from './dataSkills'
 
 import './Skills.css'
 
 function Skills() {
+  const data = dataSkills.skills;
+
   return (
     <section className="skills">
       <h2 className="skills__tittle">Tech Skills</h2>
       <section className="Section__grid">
-        <a target="_blank" rel="noreferrer" href="https://developer.mozilla.org/es/docs/Web/HTML/Element/html"><figure><img src={html} alt="HTML icon"/><figcaption><p>HTML5</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://developer.mozilla.org/es/docs/Web/CSS"><figure><img src={css} alt="CSS icon"/><figcaption><p>CSS3</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://developer.mozilla.org/es/docs/Web/JavaScript"><figure><img src={js} alt="JavaScript icon"/><figcaption><p>JavaScript</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://reactjs.org/"><figure><img src={react} alt="React js icon"/><figcaption><p>React.js</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://nodejs.org/es/"><figure><img src={node} alt="Node js icon"/><figcaption><p>Node.js</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://stylus-lang.com/"><figure><img src={stylus} alt="Stylus icon"/><figcaption><p>Stylus</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://sass-lang.com/"><figure><img src={sass} alt="Sass icon"/><figcaption><p>Sass</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://lesscss.org/"><figure><img src={less} alt="Less icon"/><figcaption><p>Less</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://webpack.js.org/"><figure><img src={webpack} alt="Webpack icon"/><figcaption><p>Webpack</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://eslint.org/"><figure><img src={eslint} alt="Eslint icon"/><figcaption><p>Eslint</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://prettier.io/"><figure><img src={prettier} alt="Prettier icon"/><figcaption><p>Prettier</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://firebase.google.com/"><figure><img src={firebase} alt="Firebase icon"/><figcaption><p>Firebase</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://git-scm.com/"><figure><img src={git} alt="Git icon"/><figcaption><p>Git</p></figcaption></figure></a>
-        <a target="_blank" rel="noreferrer" href="https://github.com/"><figure><img src={github} alt="GitHub icon"/><figcaption><p>GitHub</p></figcaption></figure></a>
+        {
+          data.map((item) => (
+            <div key={item.id}>
+              <a target="_blank" rel="noreferrer" href={item.url}>
+                <figure>
+                  <img src={item.icon} alt={item.title}/>
+                  <figcaption>
+                    <p>{item.title}</p>
+                  </figcaption>
+                </figure>
+              </a>
+            </div>
+          ))
+        }
       </section>
     </section>
   )
---
+++ src/components/Skills/dataSkills.js
@@ -0,0 +1,88 @@
+export default {
+  skills: [
+    {
+      id:  "1",
+      title: "HTML 5",
+      url: "https://developer.mozilla.org/es/docs/Web/HTML/Element/html",
+      icon: "https://i.ibb.co/Svv0NWZ/html.png"
+    },
+    {
+      id:  "2",
+      title: "CSS3",
+      url: "https://developer.mozilla.org/es/docs/Web/CSS",
+      icon: "https://i.ibb.co/MDhmCQH/css.png"
+    },
+    {
+      id:  "3",
+      title: "JavaScript",
+      url: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+      icon: "https://i.ibb.co/tCZd4cb/javascript.png"
+    },
+    {
+      id:  "4",
+      title: "React.js",
+      url: "https://reactjs.org/",
+      icon: "https://i.ibb.co/thsY9rP/react.png"
+    },
+    {
+      id:  "5",
+      title: "Nodejs",
+      url: "https://nodejs.org/es/",
+      icon: "https://i.ibb.co/h819z44/node.png"
+    },
+    {
+      id:  "6",
+      title: "Stylus",
+      url: "https://stylus-lang.com/",
+      icon: "https://i.ibb.co/jfrMkyf/stylus.png"
+    },
+    {
+      id:  "7",
+      title: "Sass",
+      url: "https://sass-lang.com/",
+      icon: "https://i.ibb.co/7J3NPV8/sass.png"
+    },
+    {
+      id:  "8",
+      title: "Less",
+      url: "https://lesscss.org/",
+      icon: "https://i.ibb.co/nBnRNXg/less.png"
+    },
+    {
+      id:  "9",
+      title: "Webpack",
+      url: "https://webpack.js.org/",
+      icon: "https://i.ibb.co/ysj1Cxh/webpack.png"
+    },
+    {
+      id:  "10",
+      title: "Eslint",
+      url: "https://eslint.org/",
+      icon: "https://i.ibb.co/2yM6WTD/eslint.png"
+    },
+    {
+      id:  "11",
+      title: "Prettier",
+      url: "https://prettier.io/",
+      icon: "https://i.ibb.co/r423DBg/prettier.png"
+    },
+    {
+      id:  "12",
+      title: "Firebase",
+      url: "https://firebase.google.com/",
+      icon: "https://i.ibb.co/vqG2h3j/firebase.png"
+    },
+    {
+      id:  "13",
+      title: "Git",
+      url: "https://git-scm.com/",
+      icon: "https://i.ibb.co/nkYZ8t3/git.png"
+    },
+    {
+      id:  "14",
+      title: "GitHub",
+      url: "https://github.com/",
+      icon: "https://i.ibb.co/D5wbfKp/github.png"
+    }
+  ]
+}
